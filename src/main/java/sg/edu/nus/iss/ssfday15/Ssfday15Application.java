15a4f057bc638b037e6da37fa51eb9013b808fe7
Dmondes <desmondlimws@gmail.com>
2024-11-22 11:00:37 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/ssfday15/Ssfday15Application.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.ssfday15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssfday15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ssfday15Application.class, args);
+	}
+
+}
